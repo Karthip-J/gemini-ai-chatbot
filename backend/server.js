3604d922a1789165351e3413cb68164d9a8a7620
Karthip <jkarthip@gmail.com>
2025-11-20 21:02:50 +0530

    Fix CORS for render deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000', // React frontend
+  origin: ['http://localhost:3000', 
+  'https://frontend-9c7bhimao-karthip-js-projects.vercel.app'],// React frontend
   credentials: true
 }));
 
